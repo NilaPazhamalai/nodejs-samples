4ba145dd3ed3a188dc03b6a1fce92cb1465a7ac4
Nila Pazhamalai <28644384+NilaPazhamalai@users.noreply.github.com>
2018-03-29 17:19:09 +0530

    verify token issue fixed

--- token-auth/auth/verifyAuth.js
+++ token-auth/auth/verifyAuth.js
@@ -2,15 +2,19 @@ var config = require('../config');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
-var verifyToken = function(req,res,next){
+var verifyToken = async function (req, res, next) {
     var accessToken = req.headers['x-access-token'];
-    if(!accessToken) 
-        return res.status(401).send({auth:false,message:'no token provided'});
-    var decoded = jwt.verify(accessToken,config.secret);
-    if(!decoded) 
-        return res.status(500).send({auth:false,message:'Failed to authenticate'});
-    req.userId = decoded.id;
-    next();
+    if (!accessToken)
+        return res.status(401).send({ auth: false, message: 'no token provided' });
+    try {
+        var decoded = await jwt.verify(accessToken, config.secret);
+        if (!decoded)
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate' });
+        req.userId = decoded.id;
+        next();
+    } catch (err) {
+        return res.status(401).send({ auth: false, message: 'Unauthorized', err: err });
+    }
 }
 
 module.exports = verifyToken;
