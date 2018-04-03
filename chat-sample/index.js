e23790e89d981f262886a3a43ee92797e376efa9
Nila Pazhamalai <28644384+NilaPazhamalai@users.noreply.github.com>
2018-04-03 20:09:52 +0530

    node_assessment - chat using websocket

---
+++ chat-sample/index.js
@@ -0,0 +1,44 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var renderChatApp = (request,response)=>{
+    response.sendFile(__dirname+"/view/"+"chat.html");
+}
+app.get('/',renderChatApp);
+
+
+var numUsers = 0;
+io.on('connection',(socket)=>{
+    var addedUser = false;
+    console.log("connection established" + socket);
+    socket.on('add_user', function(userName){
+        console.log("add user");
+        socket.userName = userName;
+        numUsers++;
+        socket.broadcast.emit('user_joined',{
+            username: socket.userName,
+            numUsers: numUsers
+          });
+        io.emit('server_msg', "ADMIN SERVER: hello "+socket.userName);
+        addedUser = true;
+    });
+
+    
+    socket.on('client_msg', function(msg){
+        io.emit('server_msg', socket.userName +" : "+msg);
+    });
+
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user_left', {
+        username: socket.userName,
+        numUsers: numUsers
+      });
+    }
+  });
+});
+  
+http.listen(8080,()=>console.log("listening at port 8080"));
